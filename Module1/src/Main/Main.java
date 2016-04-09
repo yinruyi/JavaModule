83427d793286544c9d4204a5ffc8db0710c619cc
R. Yin <916280067@qq.com>
2016-04-09 20:29:06 +0800

    Java文件的读写

--- Module1/src/Main/Main.java
+++ Module1/src/Main/Main.java
@@ -1,12 +1,19 @@
 package Main;
-import com.eclab.BinaryTree.*;
+import java.util.TreeSet;
+
+import com.eclab.util.TextFile;
+
 
 public class Main {
-
 	public static void main(String[] args) {
 		System.out.println("x");
-		new BinaryTree();
-
+		String file = TextFile.read("H://dev//JavaModule//Module1//src//com//eclab//util//TextFile.java");
+		TextFile.write("test.txt", file);
+		TextFile text = new TextFile("test.txt");
+		System.out.println(text);
+		System.out.println(text.size());
+		text.write("test2.txt");
+		TreeSet<String> words = new TreeSet<String>(new TextFile("test2.txt","\\W+"));
+		System.out.println(words.headSet("a"));
 	}
-
 }
